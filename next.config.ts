cb41a0e568a8bae94ed40726ceaa9917ca1c51db
deepak bhilala <deepak05bhilala@gmail.com>
2025-05-23 15:41:39 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 const nextConfig = {
   output: 'export',
   basePath: '/love-game',
-  assetPrefix: '/love-game/',
+  assetPrefix: '/love-game',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
